8a7d4ed5ebb95aa24e579277e11bf022a70a1941
Ankit Shaji <ankitshaji@gmail.com>
2022-06-22 20:11:43 +1200

    db connection

--- public/index.php
+++ public/index.php
@@ -6,16 +6,21 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
 require '../vendor/autoload.php';
+require '../src/config/db.php';
 
 // Define app routes
 $app = new \Slim\App;
 
-//GET request
+//Server accepts GET request
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
     return $response;
 });
+
+//Customer Routes
+require '../src/routes/customers.php';
+
 
 // Run app
 $app->run();
