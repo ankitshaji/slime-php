6e923791203dc6378967414376a0107c1185274b
Ankit Shaji <ankitshaji@gmail.com>
2022-06-22 14:07:14 +1200

    composer require slim/slim "^3.0"

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+
+require 'vendor/autoload.php';
+
+// Define app routes
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Run app
+$app->run();
