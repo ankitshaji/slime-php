a099ec7cc57b66c281f586e5ebc62e22dbd86ce3
Ankit Shaji <ankitshaji@gmail.com>
2022-06-22 15:21:13 +1200

    .htaccess - dont need to write index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,16 @@
 <?php
+//Server
+//composer require slim/slim "^3.0"
+
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 // Define app routes
 $app = new \Slim\App;
+
+//GET request
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
