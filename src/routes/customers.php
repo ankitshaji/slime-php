9a50be8f76711f53d60ac59f030eea6eee94c8f5
Ankit Shaji <ankitshaji@gmail.com>
2022-06-22 20:43:17 +1200

    Accept get request,get single customer, json

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -16,7 +16,7 @@ $app->get('/api/customers', function (Request $request, Response $response) {
         //Connect
         $db = $db->connect();
     } catch (PDOException $e) {
-        echo '{"error":'.$e->getMessage().'}';
+        echo '{"error":' . $e->getMessage() . '}';
     }
     //create statement
     $stmt = $db->query($query);
@@ -25,4 +25,28 @@ $app->get('/api/customers', function (Request $request, Response $response) {
     $db = null;
     //return json
     echo json_encode($customers);
+});
+
+//Server Accepts Get Requests - One customer
+$app->get('/api/customer/{id}', function (Request $request, Response $response) {
+    //getid
+    $id = $request->getAttribute('id');
+    //query
+    $query = "SELECT * FROM customers 
+    WHERE id = $id";
+    try {
+        //Get DB Object
+        $db = new db();
+        //Connect
+        $db = $db->connect();
+    } catch (PDOException $e) {
+        echo '{"error":' . $e->getMessage() . '}';
+    }
+    //create statement
+    $stmt = $db->query($query);
+    //Get Request to database
+    $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    //return json
+    echo json_encode($customer);
 });
