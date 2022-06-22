214f08afbdace3ba6a4024d53f63755a71c0165a
Ankit Shaji <ankitshaji@gmail.com>
2022-06-22 20:38:48 +1200

    accept getReqest,GetAll request to database,json

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -6,8 +6,23 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = new \Slim\App;
 
-//Server Accepts Get Requests
+//Server Accepts Get Requests - ALL customers
 $app->get('/api/customers', function (Request $request, Response $response) {
-    echo 'CUSTOMERS';
-    
+    //query
+    $query = "SELECT * FROM customers";
+    try {
+        //Get DB Object
+        $db = new db();
+        //Connect
+        $db = $db->connect();
+    } catch (PDOException $e) {
+        echo '{"error":'.$e->getMessage().'}';
+    }
+    //create statement
+    $stmt = $db->query($query);
+    //Get Request to database
+    $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    //return json
+    echo json_encode($customers);
 });
